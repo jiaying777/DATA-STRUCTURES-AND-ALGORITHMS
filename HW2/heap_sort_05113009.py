e2a1dac5786b00c9ab43b3ca7b02c205f039b58b
jiaying777 <55706191+jiaying777@users.noreply.github.com>
2019-11-05 12:02:47 +0800

    Update heap_sort_05113009.py

--- HW2/heap_sort_05113009.py
+++ HW2/heap_sort_05113009.py
@@ -28,7 +28,7 @@ class Solution(object):
                     list[2*i+1] = temp
                     change = True
 
-                if len(list)>=3:
+                if len(list)>=3 and 2*i+2 <= len(list)-1:
                     if list[2*i+2] > list[i] :
                         temp = list[i]
                         list[i] = list[2*i+2]
@@ -41,8 +41,6 @@ class Solution(object):
                 if 2*i+1 > len(list)-1 :
                     done = False
 
-                if 2*i+2 > len(list)-1 :
-                    done = False
 
         if change == True:
             self.heapsort(list)
